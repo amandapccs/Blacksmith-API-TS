d3833ca7f25fd85f9870df17600d3fa0f347ef90
Amanda Soares <amandchen@hotmail.com>
2022-09-20 15:54:47 -0300

    [feat]: Creates class OrdersModel with CRUD methods

---
+++ src/models/orders.model.ts
@@ -0,0 +1,33 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { Order, OrderId } from '../interfaces/order.interface';
+
+export default class OrderModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Order[]> {
+    const result = await this.connection.execute(
+      `SELECT o.id AS id, o.userId AS userId, JSON_ARRAYAGG(p.id) AS productsIds
+      FROM Trybesmith.Orders AS o
+      INNER JOIN Trybesmith.Products AS p
+      ON o.id = p.orderId
+      GROUP BY o.id
+      ORDER BY userId ASC`,
+    );
+    const [orders] = result;
+    return orders as Order[];
+  }
+
+  public async create(userId: number): Promise<OrderId> {
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Orders (userId) VALUES (?)',
+      [userId],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { orderId: insertId };
+  }
+}
